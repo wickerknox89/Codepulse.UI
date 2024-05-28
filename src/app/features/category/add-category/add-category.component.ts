92e9605157f6d835c67d7d28324a771b83b61168
wickerknox <wickerknox89@gmail.com>
2024-05-28 15:25:53 -0700

    Unsubscribe from Observables

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model : AddCategoryRequest;
+  private addCategorySubscription? : Subscription;
 
   constructor(private categoryService: CategoryService){
     this.model = { name:'', urlHandle:'' };
@@ -17,10 +19,15 @@ export class AddCategoryComponent {
 
   onFormSubmit(){
     console.log(this.model);
-    this.categoryService.addCategory(this.model).subscribe({
+    this.addCategorySubscription = this.categoryService.addCategory(this.model).subscribe({
       next: (response) => {
         console.log("This was successful");
       }
-    })
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
+
