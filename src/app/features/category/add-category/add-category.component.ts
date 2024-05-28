6751860fa793c03e308cf3f17c835a9c7b0d9de4
wickerknox <wickerknox89@gmail.com>
2024-05-28 15:19:23 -0700

    Create Angular Service - CategoryService

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-add-category',
@@ -10,11 +11,16 @@ export class AddCategoryComponent {
 
   model : AddCategoryRequest;
 
-  constructor(){
+  constructor(private categoryService: CategoryService){
     this.model = { name:'', urlHandle:'' };
   }
 
   onFormSubmit(){
     console.log(this.model);
+    this.categoryService.addCategory(this.model).subscribe({
+      next: (response) => {
+        console.log("This was successful");
+      }
+    })
   }
 }
