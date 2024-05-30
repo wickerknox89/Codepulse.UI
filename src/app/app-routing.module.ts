628d89871af011e3250e2595907f110c363e65a0
wickerknox <wickerknox89@gmail.com>
2024-05-30 09:58:31 -0700

    UI - Create 'blogpost-list' component and it's routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
+import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 
 const routes: Routes = [
   { path:'admin/categories', component: CategoryListComponent },
   { path:'admin/categories/add', component: AddCategoryComponent },
-  { path:'admin/Categories/:id', component: EditCategoryComponent }
+  { path:'admin/Categories/:id', component: EditCategoryComponent },
+  { path:'admin/blogposts', component: BlogpostListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AddCategoryComponent } from './features/category/add-category/add-categ
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
+import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { EditCategoryComponent } from './features/category/edit-category/edit-ca
     NavbarComponent,
     CategoryListComponent,
     AddCategoryComponent,
-    EditCategoryComponent
+    EditCategoryComponent,
+    BlogpostListComponent
   ],
   imports: [
     BrowserModule,
