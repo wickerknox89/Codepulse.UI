ff77baad4f67a83f2c28c32987c7492225ae8a0d
wickerknox <wickerknox89@gmail.com>
2024-05-30 15:38:21 -0700

    UI - Create 'blog-post' Service

---
+++ src/app/features/blog-post/models/blog-post.model.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+    id: string;
+    title: string;
+    shortDescription: string;
+    content: string;
+    featuredImageUrl: string;
+    urlHandle: string;
+    author: string;
+    publishedDate: Date;
+    isVisible: boolean;
+  }
+
+  
---
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPost } from '../models/blog-post.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogposts`, data);
+  }
+}
+
