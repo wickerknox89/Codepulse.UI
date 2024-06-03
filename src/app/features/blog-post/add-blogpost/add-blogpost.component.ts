355a2e5a50ed981475bb4482ffcdc9d9a98d9f25
wickerknox <wickerknox89@gmail.com>
2024-06-02 17:51:41 -0700

    UI - Display Categories MultiSelect Dropdown and Capture Categories

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
+import { CategoryService } from '../../category/services/category.service';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
   model: AddBlogPost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router){
+  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService){
     this.model = {
       title: '',
       shortDescription: '',
@@ -22,6 +26,10 @@ export class AddBlogpostComponent {
       isVisible: true,
       publishedDate: new Date()
     }
+  }
+  
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   onFormSubmit(){
