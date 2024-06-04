6e35102dfdd22ee9c3c6b93a5553fb39a09e3a51
wickerknox <wickerknox89@gmail.com>
2024-06-03 17:28:23 -0700

    UI - Fetch and Display BlogPost data in 'edit-blogpost' Component

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -10,16 +12,31 @@ import { Subscription } from 'rxjs';
 export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   routeSubscription?: Subscription;
+  getBlogPostSubscription?: Subscription;
+  model?: BlogPost;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService){}
   
   ngOnInit(): void {
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        // Get BlogPost From API
+        if (this.id){
+          this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
+            next: (response) => {
+              console.log(response);
+              this.model = response;
+            }
+          });
+          ;
+        }
       }
     })
   }
+
+  onFormSubmit(){  }
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -19,5 +19,9 @@ export class BlogPostService {
   getAllBlogPosts() : Observable<BlogPost[]> {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
   }
+
+  getBlogPostById(id: string): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
+  }
 }
 
