88e6dc3b93190f4a2ab3b559c3d702ef6fc8da72
wickerknox <wickerknox89@gmail.com>
2024-06-03 18:53:06 -0700

    UI - Show Categories and Selected Categories in 'edit-blogpost' component

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -14,10 +16,13 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   routeSubscription?: Subscription;
   getBlogPostSubscription?: Subscription;
   model?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
 
-  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService){}
+  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService, private categoryService: CategoryService){}
   
   ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
@@ -28,6 +33,8 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
             next: (response) => {
               console.log(response);
               this.model = response;
+
+              this.selectedCategories = response.categories.map(x => x.id);
             }
           });
           ;
