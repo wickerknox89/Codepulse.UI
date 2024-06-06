a5586ca9622e08e5e32d90b3bfec3414f69cdef1
wickerknox <wickerknox89@gmail.com>
2024-06-06 15:20:08 -0700

    UI - Create BlogDetails Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpo
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
 import { HomeComponent } from './features/public/home/home.component';
+import { BlogDetailsComponent } from './features/public/blog-details/blog-details.component';
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
+  { path:'blog/:url', component: BlogDetailsComponent },
   { path:'admin/categories', component: CategoryListComponent },
   { path:'admin/categories/add', component: AddCategoryComponent },
   { path:'admin/Categories/:id', component: EditCategoryComponent },
