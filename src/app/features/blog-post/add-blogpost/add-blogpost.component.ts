8b96ad31b5138bbd58e944eebd23cc6862b38dfb
wickerknox <wickerknox89@gmail.com>
2024-06-05 22:28:21 -0700

    UI - Add 'image-selector' modal pop-up to  the 'add-blogpost' Component

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Category } from '../../category/models/category.model';
 import { CategoryService } from '../../category/services/category.service';
+import { ImageService } from 'src/app/shared/components/image-selector/image.service';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent implements OnInit {
+export class AddBlogpostComponent implements OnInit, OnDestroy {
   model: AddBlogPost;
   categories$?: Observable<Category[]>;
-
-  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService){
+  isImageSelectorVisible : boolean = false;
+  imageSelectorSubscription?: Subscription;
+  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService, 
+    private imageService: ImageService){
     this.model = {
       title: '',
       shortDescription: '',
@@ -27,10 +30,21 @@ export class AddBlogpostComponent implements OnInit {
       publishedDate: new Date(),
       categories: []
     }
+}
+
+  ngOnDestroy(): void {
+    this.imageSelectorSubscription?.unsubscribe();
   }
   
   ngOnInit(): void {
     this.categories$ = this.categoryService.getAllCategories();
+
+    this.imageSelectorSubscription = this.imageService.onSelectImage().subscribe({
+      next: (selectedImage) => {
+        this.model.featuredImageUrl = selectedImage.url;
+        this.closeImageSelector();
+      }
+     })
   }
 
   onFormSubmit(){
@@ -40,6 +54,14 @@ export class AddBlogpostComponent implements OnInit {
         this.router.navigateByUrl('/admin/blogposts');
       }
     });
+  }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector() : void {
+    this.isImageSelectorVisible = false;
   }
 }
 
