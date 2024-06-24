4fa1eda989d8335743e3af80d54f8049db99ee6a
wickerknox <wickerknox89@gmail.com>
2024-06-24 14:48:15 -0700

    UI - Auth Guard and Implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,17 +9,18 @@ import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-b
 import { HomeComponent } from './features/public/home/home.component';
 import { BlogDetailsComponent } from './features/public/blog-details/blog-details.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { authGuard } from './features/auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'login', component: LoginComponent},
   { path:'blog/:url', component: BlogDetailsComponent },
-  { path:'admin/categories', component: CategoryListComponent },
-  { path:'admin/categories/add', component: AddCategoryComponent },
-  { path:'admin/Categories/:id', component: EditCategoryComponent },
-  { path:'admin/blogposts', component: BlogpostListComponent },
-  { path: 'admin/blogposts/add', component: AddBlogpostComponent},
-  { path: 'admin/blogposts/:id', component: EditBlogpostComponent}
+  { path:'admin/categories', component: CategoryListComponent, canActivate: [authGuard] },
+  { path:'admin/categories/add', component: AddCategoryComponent, canActivate: [authGuard] },
+  { path:'admin/Categories/:id', component: EditCategoryComponent, canActivate: [authGuard] },
+  { path:'admin/blogposts', component: BlogpostListComponent, canActivate: [authGuard] },
+  { path: 'admin/blogposts/add', component: AddBlogpostComponent, canActivate: [authGuard] },
+  { path: 'admin/blogposts/:id', component: EditBlogpostComponent, canActivate: [authGuard] }
 ];
 
 @NgModule({
